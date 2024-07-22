a2bebd7289a4b5e2ec2570a96f83e24d2572ff27
r.valeev <ruslan@dressx.com>
2024-07-22 19:44:51 +0200

    add User model

--- core/models.py
+++ core/models.py
@@ -1,1 +1,20 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name="Email address")
+    username = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True,
+        unique=True,
+        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        validators=[UnicodeUsernameValidator()],
+        error_messages={"unique": "A user with that username already exists."},
+        verbose_name="Username",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
