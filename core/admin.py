2bc3f934d38f2c5a30384cff4bb0f2fd57a10509
r.valeev <ruslan@dressx.com>
2024-07-31 09:28:36 +0300

    remove restriction to select only one group

--- core/admin.py
+++ core/admin.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from django.contrib.auth.forms import UserChangeForm as DjangoUserChangeForm
-from django.core.exceptions import ValidationError
 
 from .models import User
 from .sites import rentie_site
-
-
-class UserChangeForm(DjangoUserChangeForm):
-    def clean_groups(self):
-        if self.cleaned_data["groups"].count() > 1:
-            self.add_error("groups", ValidationError("Only one group allowed."))
-            return None
-        return self.cleaned_data["groups"]
 
 
 @admin.register(User, site=rentie_site)
@@ -44,7 +34,6 @@ class UserAdmin(DjangoUserAdmin):
             },
         ),
     )
-    form = UserChangeForm
     add_fieldsets = (
         (
             None,
