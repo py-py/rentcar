0b69a1e497f99ddecaa104445fc6ba9f1f4e1fff
r.valeev <ruslan@dressx.com>
2024-08-12 23:25:52 +0200

    geo: add geo application

---
+++ geo/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django_countries.fields import CountryField
+from django_extensions.db.models import TimeStampedModel
+
+from .constants import CURRENCIES
+from .constants import CURRENCY_EUR
+
+
+class Country(TimeStampedModel):
+    name = models.CharField(max_length=256, unique=True)
+    code = CountryField(max_length=2, unique=True)
+    currency = models.CharField(max_length=3, choices=CURRENCIES, default=CURRENCY_EUR)
+
+    class Meta:
+        verbose_name = "Country"
+        verbose_name_plural = "Countries"
+
+
+class City(TimeStampedModel):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
